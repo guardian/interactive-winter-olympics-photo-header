742d86e0ee6d84fbb0b9c7a6456217331eed7360
Garry Blight <garry.blight@guardian.co.uk>
2018-02-15 11:04:12 +0000

    Added share functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,10 @@
 // javascript goes here
 
+import shares from './share'
+
+let shareFn = shares('2018 Winter Olympics frame by frame', 'https://gu.com/p/85v9h', '');
+
+[].slice.apply(document.querySelectorAll('.interactive-share')).forEach(shareEl => {
+        var network = shareEl.getAttribute('data-network');
+        shareEl.addEventListener('click', () => shareFn(network));
+});
